01bb256ac69637ee15bbe57691c3a646411f6a72
Horacio Calleja <callejah@hotmail.com>
2020-10-06 10:44:20 -0300

    Probando git

--- src/instagram/Main.java
+++ src/instagram/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Usuario horacio = new Usuario("Horacio", "Calleja", "callejah@hotmail.com", "asdasd123123");
+		Usuario horacio = new Usuario("Horacio", "Calleja", "callejah@hotmail.com", "aesede");
 
 		System.out.println("Hola "+ horacio.getNombre() + " " + horacio.getApellido());
 	
