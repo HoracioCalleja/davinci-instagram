716236da46551eb050391dd537f3ab2046ec69c2
Horacio Calleja <callejah@hotmail.com>
2020-10-06 10:40:46 -0300

    primer commit

---
+++ src/instagram/Main.java
@@ -0,0 +1,14 @@
+package instagram;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Usuario horacio = new Usuario("Horacio", "Calleja", "callejah@hotmail.com", "asdasd123123");
+
+		System.out.println("Hola "+ horacio.getNombre() + " " + horacio.getApellido());
+	
+	
+	}
+
+}
---
+++ src/instagram/Usuario.java
@@ -0,0 +1,49 @@
+package instagram;
+
+public class Usuario {
+
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String contrasenia;
+
+	Usuario(String nombre, String apellido, String email, String contrasenia) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.contrasenia = contrasenia;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+}
