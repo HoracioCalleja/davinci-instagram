0f72967cb99f0c7ae4de65f0d4d9098f56e788cd
Nerfed740 <cristiandelcanto880@gmail.com>
2020-10-11 18:37:50 -0300

    Se agregó el objeto Historia

--- src/instagram/Historia.java
+++ src/instagram/Historia.java
@@ -1,5 +1,22 @@
 package instagram;
 
 public class Historia {
-
+	private Usuario usuario;
+	private Archivo contenido;
+	private int tiempoActivo;
+	private String fechaSubida;
+	private boolean  mejoresAmigos;
+	
+	
+	
+	Historia(Usuario usuario, Archivo contenido, int tiempoActivo, boolean  mejoresAmigo){
+		this.usuario = usuario;
+		this.contenido = contenido;
+		this.tiempoActivo = tiempoActivo;
+		this.fechaSubida = fechaSubida;
+		this.mejoresAmigos = mejoresAmigos;
+	}
+	public void subirHistoria(){
+		
+	}
 }
